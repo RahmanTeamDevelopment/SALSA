4a9477fa7217df150e7fc4aa8e23c053923d930f
Shawn Yost <shawn.yost@icr.ac.uk>
2018-07-27 15:16:16 +0100

    Testing CoverView-v1.4.3 installation

--- install.sh
+++ install.sh
@@ -10,6 +10,31 @@ echo ""
 echo "Installing ENSTWriter $VERSION ..."
 echo ""
 
+
+cd ${ABSOLUTE_PATH}/tools/
+wget https://github.com/RahmanTeamDevelopment/CoverView/archive/v1.4.3.tar.gz
+mv v1.4.3.tar.gz CoverView-v1.4.3.tar.gz
+tar xvf CoverView-v1.4.3.tar.gz
+rm CoverView-v1.4.3.tar.gz
+cd CoverView-1.4.3
+./install.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Installing SALSA wraper for the full set of tools
 virtualenv -p python2.7 env
 
 source ${ABSOLUTE_PATH}/env/bin/activate
