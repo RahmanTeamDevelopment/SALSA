531b496479d6060c6c10392fc09f5fde26cb0e68
Shawn Yost <shawn.yost@icr.ac.uk>
2018-07-30 15:17:43 +0100

    Update to installation. Everything ready except for Platypus

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+message () {
+echo; printf '%.0s=' {1..80}; echo
+echo "SALSA: $1"
+printf '%.0s=' {1..80}; echo; echo
+}
+
+
+
 unset PYTHONPATH
 
 ABSOLUTE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
@@ -11,6 +19,7 @@ echo "Installing SALSA $VERSION ..."
 echo ""
 
 
+message "Installing CoverView v1.4.3"
 cd ${ABSOLUTE_PATH}/tools/
 wget https://github.com/RahmanTeamDevelopment/CoverView/archive/v1.4.3.tar.gz
 mv v1.4.3.tar.gz CoverView-v1.4.3.tar.gz
@@ -18,18 +27,77 @@ tar xvf CoverView-v1.4.3.tar.gz
 rm CoverView-v1.4.3.tar.gz
 cd CoverView-1.4.3
 ./install.sh
+echo ""
 
 
+message "Installing CAVA v1.2.3"
+cd ${ABSOLUTE_PATH}/tools/
+wget https://github.com/RahmanTeam/CAVA/archive/v1.2.3.tar.gz
+mv v1.2.3.tar.gz CAVA-v1.2.3.tar.gz
+tar xvf CAVA-v1.2.3.tar.gz
+rm CAVA-v1.2.3.tar.gz
+cd CAVA-1.2.3
+./install.sh
+echo ""
 
 
+message "Installing BWA v0.7.5a"
+cd ${ABSOLUTE_PATH}/tools/
+tar xvjf src/bwa-0.7.5a.tar.bz2
+cd bwa-0.7.5a/
+make
+echo ""
 
 
+message "Installing samtools 0.1.19"
+cd ${ABSOLUTE_PATH}/tools/
+tar xvjf src/samtools-0.1.19.tar.bz2
+cd samtools-0.1.19/
+make
+echo ""
 
 
+message "Installing stampy 1.0.20"
+cd ${ABSOLUTE_PATH}/tools/
+tar zxvf src/stampy-1.0.20r1642.tgz
+cd stampy-1.0.20/
+cp makefile makefile.original
+sed 's/-Wl//g' makefile.original > makefile
+make
+echo ""
 
 
+message "Unpacking Picard"
+cd ${ABSOLUTE_PATH}/tools/
+unzip src/picard-tools-1.90.zip
+mv snappy-java-1.0.3-rc3.jar picard-tools-1.90/
 
 
+message "Installing R"
+cd ${ABSOLUTE_PATH}/tools/
+tar zxvf src/R-3.1.2.tar.gz
+cd R-3.1.2
+./configure
+make
+make check
+echo ""
+
+
+message "Installing DECoN"
+cd ${ABSOLUTE_PATH}/tools/
+tar zxvf src/DECoN_Linux_1.0.0.tar.gz
+cd DECoN_Linux_1.0.0
+mkdir packrat/src/VGAM
+cp ${ABSOLUTE_PATH}/tools/src/VGAM_0.9-8.tar.gz packrat/src/VGAM/
+[ -w ".Rprofile" ] && rm .Rprofile
+${ABSOLUTE_PATH}/tools/R-3.1.2/bin/Rscript sessionInfo.R --bootstrap-packrat > setup.log 2>&1
+cp packrat/packrat_source/.Rprofile ./
+echo ""
+
+
+message "Unpacking transcript database"
+cd ${ABSOLUTE_PATH}/
+unzip tools/src/default_transcripts.zip
 
 
 
